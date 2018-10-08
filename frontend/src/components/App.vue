85da7d5944028012149b242707b05b43db9b862c
Kuniaki Ishikawa <k.ishikawa.0128@gmail.com>
2018-10-08 18:23:19 +0900

    add counter vue.js app demo

--- frontend/src/components/App.vue
+++ frontend/src/components/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div>
     <p>Hello World from Vue.js</p>
+    <counter/>
   </div>
 </template>
+
+<script>
+import Counter from './Counter.vue'
+
+export default {
+  components: {Counter},
+}
+</script>
 
 <style>
   p {
